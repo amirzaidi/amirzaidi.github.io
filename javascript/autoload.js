7ccefacc061d6e37d169639e181983b67bd66ce0
Amir Zaidi <azaidi@live.nl>
2018-08-21 22:00:35 +0200

    Implement website
    * Modify old website and use as template
    
    * Redirect from home page to privacy policy
    
    * Fix grey background color onpress
    
    * Increase max height for the collapsing text box
    
    * Write draft for PRIVACY.md
    
    * Change image links to HTTPS

---
+++ javascript/autoload.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+	var client = new XMLHttpRequest()
+	client.open('GET', 'PRIVACY.md')
+	client.onreadystatechange = function() {
+		var response = client.responseText.split('\n')
+		var summary = response.shift()
+		var full = response.join('\n\n').trim()
+		$('#js-summary').html(summary)
+		$('#js-full').html(full)
+	}
+	client.send()
+})
